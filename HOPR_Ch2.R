6a0709bd33488a33fc4c36c9fbd04d37dcff54bf
Mike Loranty <mloranty@colgate.edu>
2019-02-01 09:52:28 -0500

    Added script to go through portions of Chapter 2 in 'Hands-On Programming with R, and updated exercise 1.

---
+++ HOPR_Ch2.R
@@ -0,0 +1,16 @@
+
+
+# arithmetic in R
+1+1
+
+# use the ':' to return very integer between two integers
+# also the brackets denote which the place in a sequence of integers is at the start of each line
+100:130
+
+# if you see a '+' sign, R is waiting for you to complete the statement
+5-
+  
+
+# if R does not recognize your statement it will return an error message
+3 % 5
+
