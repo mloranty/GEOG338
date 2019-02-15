198710a9553c37321402c0ebce483a59bfbc7396
Mike Loranty <mloranty@colgate.edu>
2019-02-15 13:05:22 -0500

    Added lab 2 exercises and solution.

--- Lab1_solution.R
+++ Lab1_solution.R
@@ -1,18 +1,21 @@
 # solutions to Lab 1 questions
 
-library(dplyr)
-library(tidyr)
-
-
 # 1.   from the same data webpage load the *net radiation* data into R
 
 nr <- read.csv("https://arcticdata.io/metacat/d1/mn/v2/object/urn%3Auuid%3A229ad93b-ddfe-4e3f-a32d-d8e8e17eca4d",
                header = T)
 
-nr <- read.csv("https://cn.dataone.org/cn/v2/resolve/urn:uuid:229ad93b-ddfe-4e3f-a32d-d8e8e17eca4d")
 
 # 2.   convert the downward facing longwave radiation data to temperature using the Stefan-Boltzman equation
-
+sb <- 5.67*10^-8
+lst <- (nr$IRDn/(0.96*sb))^0.25
 
 # 3.   calculate the minimum, maximum, and average radiometric land surface temperature
-nr[nr$site=="hd" & nr$sensorZ==800,]
+min(lst, na.rm = T)
+# 247.0301
+
+max(lst, na.rm = T)
+# 312.0318
+
+mean(lst, na.rm = T)
+#280.6988
---
+++ Lab2_solution.R
@@ -0,0 +1,4 @@
+
+
+vwc <- read.csv("https://arcticdata.io/metacat/d1/mn/v2/object/urn%3Auuid%3A091841a5-ff1c-4465-8646-57d136085c61",
+                header = T)
