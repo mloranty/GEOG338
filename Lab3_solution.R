15dd327c0511b1226e3ecf905be55945b03cd319
Mike Loranty <mloranty@colgate.edu>
2019-03-08 13:10:38 -0500

    Added exercise 4

--- Lab3_solution.R
+++ Lab3_solution.R
@@ -3,3 +3,32 @@
 nr <- read.csv("https://arcticdata.io/metacat/d1/mn/v2/object/urn%3Auuid%3A229ad93b-ddfe-4e3f-a32d-d8e8e17eca4d",
                header = T)
 
+
+# create a new object called t_month that is identical to our t_air dataframe and pipe it to the next line
+nr_day <- nr %>%
+  
+  # group the data by year, month, site, and sensor height, and pipe to the next line  
+  group_by(year,doy,site,sensorZ) %>%
+  
+  # summarize data - calculate the mean and standard deviation for number of observations for each month
+  summarize(sw = mean(SRUp,na.rm=T),
+            sw.sd = sd(SRUp,na.rm=T),
+            n_obs <- n())
+
+# check out the new data frame
+head(nr_day)
+hd1 <- nr_day[nr_day$year == 2017 & nr_day$doy < 182 & nr_day$doy > 151 & nr_day$site == "hd" & nr_day$sensorZ == 100,]
+hd8 <- nr_day[nr_day$year == 2017 & nr_day$doy < 182 & nr_day$doy > 151 & nr_day$site == "hd" & nr_day$sensorZ == 800,]
+ld1 <- nr_day[nr_day$year == 2017 & nr_day$doy < 182 & nr_day$doy > 151 & nr_day$site == "ld" & nr_day$sensorZ == 100,]
+ld8 <- nr_day[nr_day$year == 2017 & nr_day$doy < 182 & nr_day$doy > 151 & nr_day$site == "ld" & nr_day$sensorZ == 800,]
+
+plot(ld8$doy, ld8$sw,lty=1,lwd=2,type = "l",
+     ylim = c(0,380),
+     xlab = "Day of Year",
+     ylab = expression(paste("SW (W ",m^-2,")",sep="")))
+
+lines(ld1$doy, ld1$sw,lty=2,lwd=2)
+lines(hd1$doy, hd1$sw,lty=2,lwd=2,col="red")
+lines(hd8$doy, hd8$sw,lty=1,lwd=2,col="red")
+
+legend("bottom",c("High Density","Low Density"),fill=c("red", "black"),bty="n")
---
+++ Lab4_solution.R
@@ -0,0 +1,34 @@
+
+
+c <- read.csv("https://arcticdata.io/metacat/d1/mn/v2/object/urn%3Auuid%3Ae83680e9-0579-4b5d-87df-0665dc27cbc0",header = T,skip = 1,na.strings = "N/A")
+
+
+# plot CO2 flux vs. PAR to examine light response curve
+plot(c$PAR.umol.m2.s.,c$CO2.flux..umol.m2.s.,
+     ylim = c(10,-7))
+
+flux <- as.data.frame(cbind(c$PAR.umol.m2.s.,-c$CO2.flux..umol.m2.s.))
+names(flux) <- c("par","co2")
+
+
+test <- nls(co2~((Jmax*par*Amax)/(Jmax*par+Amax)),data = flux,
+            start = list(Amax = 1,Jmax = 1))
+            
+# Amax = maximum rate of photosynthesis
+# K = light extinction coefficient
+# b = Amax/E, where E = light use efficiency
+# 3.5 = LAI for our stand
+
+par <- c$PAR.umol.m2.s.
+co2 <- -c$CO2.flux..umol.m2.s.
+
+
+# plot CO2 flux vs. PAR to examine light response curve
+plot(c$Air.temperature,c$CO2.flux..umol.m2.s.,
+     ylim = c(10,-7))
+
+# plot dark CO2 flux vs air temp
+plot(c$Soil.temperature.at.site.1[c$PAR.umol.m2.s.<30],
+     c$CO2.flux..umol.m2.s.[c$PAR.umol.m2.s.<30])
+
+plot(c$Latent.heat.flux.W.m2.,-c$CO2.flux..umol.m2.s.)
